9ea1d024c0d3f0d182ac86049d7548e4446dd7f9
ductoan2732000 <48398070+ductoan2732000@users.noreply.github.com>
2021-01-20 22:13:51 +0700

    change 20/01 10h13pm

--- src/store/test.js
+++ src/store/test.js
@@ -1,3 +1,10 @@
 let a = "toan";
 let b = "huwng";
 console.log([a, b]);
+/***
+ * đây là một file js
+ * để run file js này trực tiếp mà ko liên quan đến các file khác hoặc ko ảnh hưởng gì đến project hiện tại 
+ * ta dùng: 
+ * vì javasscript phải dùng node.js để setup môi trường nên gõ lệnh terminal: node teenfile.js
+ * 
+ */
